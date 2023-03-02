c7588ca802c200ec7b604b4e7deac29eaaa08d74
Wilson Gouanet <wilstito@gmail.com>
2023-03-02 04:49:38 -0500

    Add profile photo to personal information

--- components/PersonalInformation.vue
+++ components/PersonalInformation.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <h1 class="mb-1">{{ data.name }}</h1>
-    <h6 class="text-primary mb-3 font-weight-normal">{{ $t(data.title) }}</h6>
-    <vue-showdown :markdown="$t(data.bio)" />
+  <div class='row'>
+    <div class='col-12 col-md-9'>
+      <h1 class="mb-1">{{ data.name }}</h1>
+      <h6 class="text-primary mb-3 font-weight-normal">{{ $t(data.title) }}</h6>
+      <vue-showdown :markdown="$t(data.bio)" />
+    </div>
+    <div class='col-12 col-md-3 d-flex align-items-center'>
+      <img alt="Wilson Gouanet" src='~/assets/images/profile.jpg' class='img-fluid'>
+    </div>
   </div>
 </template>
 
