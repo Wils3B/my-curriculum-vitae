94932ead2df140ab6ca2fae254f5e5fd291a9ce4
Wilson Gouanet <wilstito@gmail.com>
2023-03-02 04:47:46 -0500

    Add turing profile to contacts

---
+++ components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contact">
+    <Icon :icon="contact.icon" class="text-primary" />
+    <a v-if="contact.link" :href="contact.link" target="_blank" class="text-black">
+      {{ $t(contact.value) }}
+    </a>
+    <span v-else>{{ $t(contact.value) }}</span>
+  </div>
+</template>
+
+<script>
+import { Icon } from '@iconify/vue2';
+
+export default {
+  name: 'Contact',
+  props: {
+    contact: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: { Icon },
+};
+</script>
+
+<style scoped>
+.contact {
+  padding: 0.4rem 0.4rem;
+}
+</style>
