3f95dc81f98ea816c23f66719334f50443ee699d
Wilson Gouanet <wilstito@gmail.com>
2023-03-02 04:42:51 -0500

    Turn achievements into two per line

--- components/Achievements.vue
+++ components/Achievements.vue
@@ -1,10 +1,10 @@
 <template>
-  <ul class="list-unstyled">
-    <li class="py-2 mb-0" v-for="achievement of achievements" :key="achievement.title">
+  <div class="row">
+    <div class="py-2 mb-0 col-12 col-md-6" v-for="achievement of achievements" :key="achievement.title">
       <h5 class="fw-medium">{{ $t(achievement.title) }}</h5>
       <p class="mb-0 text-light">{{ $t(achievement.description) }}</p>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 
 <script>
